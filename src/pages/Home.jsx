5cd7e42c266b3c61c9f9eba3cf303864a0e1988c
Keren Duchan <keren.duchan@gmail.com>
2023-11-15 16:11:36 +0200

    svg icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,34 @@
 import { Link } from 'react-router-dom'
 
 export function Home() {
+    const icons = [
+        'people',
+        'star',
+        'board',
+        'filter',
+        'more',
+        'add',
+        'down',
+        'clock',
+        'attach',
+        'description',
+    ]
+
+    const trellicons = []
+
     return (
         <div id="home-page">
             <h1>Home page</h1>
             <Link to="/login">Log In</Link>
+
+            <h1>Icons</h1>
+            <ul style={{ display: 'flex', gap: '10px' }}>
+                {icons.map((icon) => (
+                    <li key={icon}>
+                        <img src={`images/${icon}.svg`} />
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
