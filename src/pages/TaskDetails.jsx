f9948126859bbee304bdf2c29c813dd62b117cae
Keren Duchan <keren.duchan@gmail.com>
2023-11-17 14:57:49 +0200

    wip styling: task details

--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -1,8 +1,12 @@
 export function TaskDetails({ task, onClose }) {
     return (
-        <div className="task-details">
-            <button onClick={onClose}>Close</button>
-            task details
+        <div className="task-details-bg">
+            <div className="task-details">
+                <button className="circle-btn close-btn" onClick={onClose}>
+                    <img src="images/close.svg" alt="close" />
+                </button>
+                <h1 className="title">{task.title}</h1>
+            </div>
         </div>
     )
 }
