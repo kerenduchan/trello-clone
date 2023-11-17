184cd4cfa641b3332cb8563453c4f6e682a46544
Keren Duchan <keren.duchan@gmail.com>
2023-11-17 08:38:07 +0200

    BoardDetailsTopbar component

---
+++ src/cmp/BoardDetailsTopbar.jsx
@@ -0,0 +1,7 @@
+export function BoardDetailsTopbar({ board }) {
+    return (
+        <header className="board-details-topbar">
+            <h1>{board.title}</h1>
+        </header>
+    )
+}
--- src/pages/BoardDetails.jsx
+++ src/pages/BoardDetails.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { boardService } from '../services/board.service'
 import { TaskGroup } from '../cmp/TaskGroup'
+import { BoardDetailsTopbar } from '../cmp/BoardDetailsTopbar'
 
 export function BoardDetails() {
     const [board, setBoard] = useState(null)
@@ -24,7 +25,7 @@ export function BoardDetails() {
 
     return (
         <div className="board-details">
-            <h1>{board.title}</h1>
+            <BoardDetailsTopbar board={board} />
             <ul>
                 {board.taskGroups.map((g) => (
                     <li key={g._id}>
