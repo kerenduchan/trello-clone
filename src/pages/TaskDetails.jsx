9b9e4ba392481cbddefce53921ebe64b979067f0
Keren Duchan <keren.duchan@gmail.com>
2023-11-18 13:57:17 +0200

    wip styling for task details

--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -13,16 +13,20 @@ export function TaskDetails({ task }) {
         <div className="task-details-bg" onClick={onClose}>
             <div className="task-details" onClick={(e) => e.stopPropagation()}>
                 <CircleButton type="close" onClick={onClose} />
+                {task.cover && (
+                    <div
+                        className="cover"
+                        style={{
+                            backgroundColor: task.cover.bgColor,
+                        }}
+                    ></div>
+                )}
                 <div className="header">
-                    {task.cover && (
-                        <div
-                            className="cover"
-                            style={{
-                                backgroundColor: task.cover.bgColor,
-                            }}
-                        ></div>
-                    )}
+                    <div className="icon material-symbols-outlined">
+                        credit_card
+                    </div>
                     <h1 className="title">{task.title}</h1>
+                    <p className="subtitle">in list...</p>
                 </div>
             </div>
         </div>
