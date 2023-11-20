ad83c110c3922be6308cbf5aea65950720ea37ca
Keren Duchan <keren.duchan@gmail.com>
2023-11-20 09:26:31 +0200

    fix: use className, not class, in jsx file

--- src/cmp/general/ProgressBar.jsx
+++ src/cmp/general/ProgressBar.jsx
@@ -1,9 +1,12 @@
 export function ProgressBar({ percent }) {
     return (
-        <div class="progress">
-            <span class="label">{percent}</span>
-            <div class="bar">
-                <div class="completed" style={{ width: `${percent}%` }}></div>
+        <div className="progress">
+            <span className="label">{percent}</span>
+            <div className="bar">
+                <div
+                    className="completed"
+                    style={{ width: `${percent}%` }}
+                ></div>
             </div>
         </div>
     )
