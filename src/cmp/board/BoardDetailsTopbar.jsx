b7e24ca247b6825390671eb6d1f7057996dfbe77
Keren Duchan <keren.duchan@gmail.com>
2023-11-21 12:45:52 +0200

    styling for edit board name

--- src/cmp/board/BoardDetailsTopbar.jsx
+++ src/cmp/board/BoardDetailsTopbar.jsx
@@ -34,6 +34,7 @@ export function BoardDetailsTopbar({ board }) {
                     name="title"
                     onChange={handleChange}
                     value={draft.title}
+                    onBlur={onSubmit}
                 />
             </form>
         </header>
