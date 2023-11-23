5919464a0741b761001b9431b190af515d1380c1
Keren Duchan <keren.duchan@gmail.com>
2023-11-23 17:36:27 +0200

    wip task label menu item

--- src/cmp/task/TaskLabelsMenu.jsx
+++ src/cmp/task/TaskLabelsMenu.jsx
@@ -1,19 +1,15 @@
 import { TaskLabelsMenuItem } from './TaskLabelsMenuItem'
 
 export function TaskLabelsMenu({ board, task }) {
-    function isLabelUsed(labelId) {
-        // todo
-        return false
-    }
-
     return (
         <div className="task-labels-menu">
             <ul>
                 {board.labels.map((label) => (
                     <li key={label._id}>
                         <TaskLabelsMenuItem
+                            board={board}
+                            task={task}
                             label={label}
-                            isChecked={isLabelUsed(label._id)}
                         />
                     </li>
                 ))}
--- src/cmp/task/TaskLabelsMenuItem.jsx
+++ src/cmp/task/TaskLabelsMenuItem.jsx
@@ -1,9 +1,21 @@
 import { SquareIconBtn } from '../general/btn/SquareIconBtn'
 
-export function TaskLabelsMenuItem({ label, isChecked }) {
+export function TaskLabelsMenuItem({ board, task, label }) {
+    function onCheckboxChange(e) {
+        if (e.target.checked) {
+            // add label to task
+            //task.labelIds = [...task.labelIds, label._id]
+        } else {
+        }
+    }
+
     return (
         <div className="task-labels-menu-item">
-            <input type="checkbox" />
+            <input
+                className="checkbox"
+                type="checkbox"
+                onChange={onCheckboxChange}
+            />
             <div className="label" style={{ backgroundColor: label.color }}>
                 {label.title}
             </div>
