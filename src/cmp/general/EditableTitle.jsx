ffb2d63b3f1ce84de048dbe04fb054b3e8ac6146
Keren Duchan <keren.duchan@gmail.com>
2023-11-23 09:08:57 +0200

    styling/refactor: fix/improve EditableTitle

--- src/cmp/general/EditableTitle.jsx
+++ src/cmp/general/EditableTitle.jsx
@@ -1,25 +1,53 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { buildClassName } from '../../util'
-import { TitleEditForm } from '../general/TitleEditForm'
+import { useForm } from '../../customHooks/useForm'
 
 export function EditableTitle({ title, onChange }) {
     const [showForm, setShowForm] = useState(false)
+    const [draft, handleChange] = useForm({ title })
+    const textareaRef = useRef(null)
+    const titleRef = useRef(null)
 
-    function onTitleClick() {
-        setShowForm(true)
-    }
+    useEffect(() => {
+        if (showForm) {
+            textareaRef.current.select()
+        }
+    }, [showForm])
 
-    function onSubmit(draft) {
+    function onSubmit(e) {
+        e.preventDefault()
         onChange(draft.title)
         setShowForm(false)
     }
 
+    function onKeyDown(e) {
+        if (e.key === 'Enter') {
+            onSubmit(e)
+        }
+    }
+
     return (
         <div className={buildClassName('editable-title', showForm && 'edit')}>
-            <span className="title" onClick={onTitleClick}>
-                {title}
+            <span
+                className="title"
+                ref={titleRef}
+                onClick={() => setShowForm(true)}
+            >
+                {draft.title}
             </span>
-            {showForm && <TitleEditForm title={title} onSubmit={onSubmit} />}
+            <form onSubmit={onSubmit}>
+                {/* using a textarea and not an input for more robust styling */}
+                <textarea
+                    rows="1"
+                    ref={textareaRef}
+                    type="text"
+                    name="title"
+                    onChange={handleChange}
+                    onKeyDown={onKeyDown}
+                    value={draft.title}
+                    onBlur={onSubmit}
+                />
+            </form>
         </div>
     )
 }
