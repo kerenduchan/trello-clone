3ec41c412e53be8a02a09e1ea37dab51d14313b7
Keren Duchan <keren.duchan@gmail.com>
2023-11-23 12:16:05 +0200

    wip edit button on hover for task preview

--- src/cmp/task/TaskPreview.jsx
+++ src/cmp/task/TaskPreview.jsx
@@ -1,19 +1,24 @@
 import { Link } from 'react-router-dom'
+import { CircleBtn } from '../general/btn/CircleBtn'
 
 export function TaskPreview({ board, task }) {
+    function onEditClick() {
+        console.log('edit')
+    }
+
     return (
-        <Link to={`/b/${board._id}/c/${task._id}`}>
-            <section className="task-preview">
-                {task.cover && (
-                    <div
-                        className="cover"
-                        style={{
-                            backgroundColor: task.cover.bgColor,
-                        }}
-                    ></div>
-                )}
-                <p className="title">{task.title}</p>
-            </section>
-        </Link>
+        <section className="task-preview">
+            {task.cover && (
+                <div
+                    className="cover"
+                    style={{
+                        backgroundColor: task.cover.bgColor,
+                    }}
+                ></div>
+            )}
+            <p className="title">{task.title}</p>
+            <Link className="link" to={`/b/${board._id}/c/${task._id}`} />
+            <CircleBtn type="edit" onClick={onEditClick} />
+        </section>
     )
 }
