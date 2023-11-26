25527d00b6715b0877da9c1872bff2e15cb1379b
Keren Duchan <keren.duchan@gmail.com>
2023-11-26 15:48:35 +0200

    styling: progress bar green when 100%

--- src/cmp/general/ProgressBar.jsx
+++ src/cmp/general/ProgressBar.jsx
@@ -1,10 +1,15 @@
+import { buildClassName } from '../../util'
+
 export function ProgressBar({ percent }) {
     return (
         <div className="progress-bar">
             <span className="label">{percent}</span>
             <div className="bar">
                 <div
-                    className="completed"
+                    className={buildClassName(
+                        'completed-part',
+                        percent === 100 && 'fully-complete'
+                    )}
                     style={{ width: `${percent}%` }}
                 ></div>
             </div>
