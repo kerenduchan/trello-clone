04b1302433ed94688412b17e39b37838e6a16bff
Keren Duchan <keren.duchan@gmail.com>
2023-11-26 16:16:24 +0200

    refactor: use generic DeleteMenu component

--- src/cmp/task/TaskDetailsSidebar.jsx
+++ src/cmp/task/TaskDetailsSidebar.jsx
@@ -1,12 +1,26 @@
+import { useNavigate } from 'react-router'
 import { usePopoverState } from '../../customHooks/usePopoverState'
+import { boardService } from '../../services/board.service'
+import { updateBoard } from '../../store/actions/board.actions'
+import { deepClone } from '../../util'
+import { DeleteMenu } from '../general/DeleteMenu'
 import { SecondaryBtn } from '../general/btn/SecondaryBtn'
-import { PopoverMenu } from '../general/PopoverMenu'
-import { TaskDelete } from './TaskDelete'
 import { TaskLabelsMenu } from './TaskLabelsMenu'
 
 export function TaskDetailsSidebar({ board, group, task }) {
+    const navigate = useNavigate()
     const labelsMenu = usePopoverState()
     const deleteTaskMenu = usePopoverState()
+
+    function onDeleteTask() {
+        // remove this task from the group in the board
+        const boardClone = deepClone(board)
+        const groupClone = boardService.getGroupById(boardClone, group._id)
+        groupClone.tasks = groupClone.tasks.filter((t) => t._id !== task._id)
+        updateBoard(boardClone)
+        deleteTaskMenu.onClose()
+        navigate(`/b/${board._id}`)
+    }
 
     return (
         <>
@@ -63,14 +77,12 @@ export function TaskDetailsSidebar({ board, group, task }) {
 
             {/* Delete task menu */}
             {deleteTaskMenu.show && (
-                <PopoverMenu title="Delete Card?" {...deleteTaskMenu.popover}>
-                    <TaskDelete
-                        board={board}
-                        group={group}
-                        task={task}
-                        onClose={deleteTaskMenu.onClose}
-                    />
-                </PopoverMenu>
+                <DeleteMenu
+                    deleteMenu={deleteTaskMenu}
+                    title="Delete Card?"
+                    text="All actions will be removed from the activity feed and you won't be able to re-open the card. There is no undo."
+                    onDelete={onDeleteTask}
+                />
             )}
         </>
     )
