2d0b40d100647ec4fdd6a5798fa8dded61e4a45d
Keren Duchan <keren.duchan@gmail.com>
2023-11-26 16:53:05 +0200

    styling: fix regression of description subsection in task details

--- src/cmp/task/TaskDetailsDescription.jsx
+++ src/cmp/task/TaskDetailsDescription.jsx
@@ -4,16 +4,15 @@ import { TaskDetailsSubsectionHeader } from './TaskDetailsSubsectionHeader'
 export function TaskDetailsDescription({ task }) {
     return (
         <div className="task-details-description">
-            <TaskDetailsSubsectionHeader
-                icon="description"
-                title="Description"
-            />
+            <TaskDetailsSubsectionHeader icon="description" title="Description">
+                {task.description && (
+                    <SecondaryBtn className="title-btn" text="Edit" />
+                )}
+            </TaskDetailsSubsectionHeader>
+
             <div className="content">
                 {task.description ? (
-                    <>
-                        <SecondaryBtn className="title-btn" text="Edit" />
-                        <p className="text">{task.description}</p>
-                    </>
+                    <p className="text">{task.description}</p>
                 ) : (
                     <SecondaryBtn
                         className="add-btn"
