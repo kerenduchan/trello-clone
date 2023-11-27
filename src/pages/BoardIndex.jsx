69b589871df737aa043ae28458bedc13f99fcdc4
Keren Duchan <keren.duchan@gmail.com>
2023-11-27 08:57:19 +0200

    feature: show starred boards in board index

--- src/pages/BoardIndex.jsx
+++ src/pages/BoardIndex.jsx
@@ -1,24 +1,42 @@
-import { useEffect } from 'react'
-import { useParams } from 'react-router'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { loadBoards } from '../store/actions/board.actions'
 import { BoardList } from '../cmp/board/BoardList'
+import { Icon } from '../cmp/general/Icon'
 
 export function BoardIndex() {
-    const params = useParams()
     const boards = useSelector((storeState) => storeState.boardModule.boards)
+    const [starredBoards, setStarredBoards] = useState([])
 
     useEffect(() => {
         loadBoards()
     }, [])
 
+    useEffect(() => {
+        setStarredBoards(boards.filter((b) => b.isStarred === true))
+    }, [boards])
+
     if (!boards) return <div>Loading..</div>
 
     return (
         <div className="board-index">
-            <div className="board-list-container">
-                <h1 className="board-list-title">Your Boards</h1>
-                <BoardList boards={boards} />
+            <div className="boards-sections">
+                {/* Starred boards */}
+                {!!starredBoards?.length && (
+                    <div className="boards-section">
+                        <header className="header">
+                            <Icon type="star" size="lg"></Icon>
+                            <h1 className="title">Starred Boards</h1>
+                        </header>
+                        <BoardList boards={starredBoards} />
+                    </div>
+                )}
+
+                {/* All boards */}
+                <div className="your-boards-section">
+                    <h1 className="title">Your boards</h1>
+                    <BoardList boards={boards} />
+                </div>
             </div>
         </div>
     )
