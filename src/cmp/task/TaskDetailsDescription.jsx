6136857a4e7f6f1876e9d922feac49a49e7d5edf
Keren Duchan <keren.duchan@gmail.com>
2023-11-30 10:20:45 +0200

    wip feature: edit task description

--- src/cmp/task/TaskDetailsDescription.jsx
+++ src/cmp/task/TaskDetailsDescription.jsx
@@ -1,25 +1,38 @@
+import { useState } from 'react'
 import { SecondaryBtn } from '../general/btn/SecondaryBtn'
 import { TaskDetailsSubsectionHeader } from './TaskDetailsSubsectionHeader'
+import { TaskDetailsDescriptionForm } from './TaskDetailsDescriptionForm'
 
 export function TaskDetailsDescription({ task }) {
+    const [showForm, setShowForm] = useState(false)
+
+    // the content is either the task description, the edit form, or the
+    // large add description button
+    function getContent() {
+        if (showForm) {
+            return <TaskDetailsDescriptionForm />
+        }
+        if (task.description) {
+            return <p className="text">{task.description}</p>
+        }
+        return (
+            <SecondaryBtn
+                className="add-btn"
+                text="Add a more detailed description..."
+                onClick={() => setShowForm(true)}
+            />
+        )
+    }
+
     return (
         <div className="task-details-description">
             <TaskDetailsSubsectionHeader icon="description" title="Description">
-                {task.description && (
+                {task.description && !showForm && (
                     <SecondaryBtn className="title-btn" text="Edit" />
                 )}
             </TaskDetailsSubsectionHeader>
 
-            <div className="content">
-                {task.description ? (
-                    <p className="text">{task.description}</p>
-                ) : (
-                    <SecondaryBtn
-                        className="add-btn"
-                        text="Add a more detailed description..."
-                    />
-                )}
-            </div>
+            <div className="content">{getContent()}</div>
         </div>
     )
 }
---
+++ src/cmp/task/TaskDetailsDescriptionForm.jsx
@@ -0,0 +1,3 @@
+export function TaskDetailsDescriptionForm() {
+    return <div>form</div>
+}
