08c123969065383d5fbc26a1f83f94dcafe46458
Keren Duchan <keren.duchan@gmail.com>
2023-12-01 07:15:01 +0200

    close checklist forms when clicking outside the checklists section

--- src/cmp/task/checklist/ChecklistList.jsx
+++ src/cmp/task/checklist/ChecklistList.jsx
@@ -1,9 +1,34 @@
+import { useRef } from 'react'
+import {
+    setCurChecklist,
+    setCurChecklistItem,
+} from '../../../store/actions/app.actions'
 import { Checklist } from './Checklist'
+import { useEffect } from 'react'
+import { useCallback } from 'react'
 
 export function ChecklistList({ hierarchy }) {
     const { task } = hierarchy
+    const listElRef = useRef()
+
+    useEffect(() => {
+        document.addEventListener('mousedown', mouseDownListener)
+
+        return () => {
+            document.removeEventListener('mousedown', mouseDownListener)
+        }
+    })
+
+    const mouseDownListener = useCallback((e) => {
+        if (listElRef.current && !listElRef.current.contains(e.target)) {
+            // clicked outside of list
+            setCurChecklist(null)
+            setCurChecklistItem(null)
+        }
+    })
+
     return (
-        <div className="checklist-list">
+        <div className="checklist-list" ref={listElRef}>
             {task.checklists && (
                 <ol>
                     {task.checklists.map((checklist) => (
