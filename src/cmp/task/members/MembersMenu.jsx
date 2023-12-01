9c81da2e398a3a352c3ebbd011f2ceccd0688fff
Keren Duchan <keren.duchan@gmail.com>
2023-12-01 15:13:15 +0200

    fix error when board has no members

--- src/cmp/task/members/MembersMenu.jsx
+++ src/cmp/task/members/MembersMenu.jsx
@@ -12,7 +12,7 @@ export function MembersMenu({ hierarchy, popoverState }) {
             <h4>Board members</h4>
 
             <ul>
-                {board.members.map((member) => (
+                {board.members?.map((member) => (
                     <li key={member._id}>
                         <MemberBtn hierarchy={hierarchy} member={member} />
                     </li>
