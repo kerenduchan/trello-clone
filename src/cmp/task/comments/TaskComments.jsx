8299ea1655e588660fea3ab1236955caf9219dd5
Keren Duchan <keren.duchan@gmail.com>
2023-12-02 16:25:05 +0200

    wip: task comments form

--- src/cmp/task/comments/TaskComments.jsx
+++ src/cmp/task/comments/TaskComments.jsx
@@ -1,18 +1,44 @@
 import { useState } from 'react'
 import { TaskCommentsForm } from './TaskCommentsForm'
+import { useForm } from '../../../customHooks/useForm'
+import { boardService } from '../../../services/board.service'
+import { useKeyDownListener } from '../../../customHooks/useKeyDownListener'
 
 export function TaskComments({ hierarchy }) {
+    const { task } = hierarchy
+
+    // TODO: show form and comment if comment draft exists on the task
     const [showForm, setShowForm] = useState(false)
+
+    const [draft, handleChange, setDraft] = useForm({ text: '' })
+
+    useKeyDownListener(['Escape'], onHideForm)
+
+    function onHideForm() {
+        setShowForm(false)
+    }
+
+    function onShowForm() {
+        setShowForm(true)
+    }
+
+    function onSubmitForm() {
+        // addComment(hierarchy, draft)
+        setDraft({ text: '' })
+        onHideForm()
+    }
 
     return (
         <div className="task-comments">
             {showForm ? (
-                <TaskCommentsForm hierarchy={hierarchy} />
+                <TaskCommentsForm
+                    draft={draft}
+                    handleChange={handleChange}
+                    onClose={onHideForm}
+                    onSubmit={onSubmitForm}
+                />
             ) : (
-                <button
-                    className="create-btn"
-                    onClick={() => setShowForm(true)}
-                >
+                <button className="create-btn" onClick={onShowForm}>
                     Write a comment...
                 </button>
             )}
--- src/cmp/task/comments/TaskCommentsForm.jsx
+++ src/cmp/task/comments/TaskCommentsForm.jsx
@@ -1,3 +1,22 @@
-export function TaskCommentsForm({ hierarchy }) {
-    return <form>form</form>
+export function TaskCommentsForm({ draft, handleChange, onClose, onSubmit }) {
+    function onSubmitInternal(e) {
+        e.preventDefault()
+        onSubmit()
+    }
+    return (
+        <form className="task-comments-form" onSubmit={onSubmitInternal}>
+            <textarea
+                type="text"
+                name="text"
+                onChange={handleChange}
+                value={draft.text}
+            />
+            <button
+                className="btn-primary btn-save"
+                disabled={draft.text.length === 0}
+            >
+                Save
+            </button>
+        </form>
+    )
 }
