5a7b8ac5f75fd963467ba45cd7da3d8211a10b5c
Keren Duchan <keren.duchan@gmail.com>
2023-12-05 08:12:12 +0200

    wip home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom'
+import { HomeHeader } from '../cmp/home/HomeHeader'
 
 export function Home() {
     return (
-        <div id="home-page">
-            <h1>Home page</h1>
-            <Link to="/login">Log In</Link>
+        <div id="home">
+            <HomeHeader />
+            <section className="content">
+                <h1>Home page</h1>
+                <Link to="/login">Log In</Link>
+            </section>
         </div>
     )
 }
