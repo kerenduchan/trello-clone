40488757b4157d79989c0793f485fddeb38521db
Keren Duchan <keren.duchan@gmail.com>
2023-12-06 16:37:04 +0200

    styling: fixed header in home page

--- src/cmp/home/HomeHeader.jsx
+++ src/cmp/home/HomeHeader.jsx
@@ -2,12 +2,14 @@ import { Link } from 'react-router-dom'
 
 export function HomeHeader() {
     return (
-        <header className="home-header">
-            <img className="logo" src="krello.svg" />
-            <div className="login-btns">
-                <Link to="/boards" className="btn-go-to-your-boards">
-                    Go to your boards
-                </Link>
+        <header className="home-header-bg">
+            <div className="home-header">
+                <img className="logo" src="krello.svg" />
+                <div className="login-btns">
+                    <Link to="/boards" className="btn-go-to-your-boards">
+                        Go to your boards
+                    </Link>
+                </div>
             </div>
         </header>
     )
