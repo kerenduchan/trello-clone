350cd6dc3daeda60052962c34b79dac65742d7e1
Keren Duchan <keren.duchan@gmail.com>
2023-12-06 08:58:51 +0200

    styling for home 101

--- src/cmp/home/Home101.jsx
+++ src/cmp/home/Home101.jsx
@@ -28,14 +28,16 @@ export function Home101() {
     return (
         <section className="home-101-bg">
             <div className="home-101">
-                <p className="home-section-title">Krello 101</p>
-                <h2>A productivity powerhouse</h2>
-                <p>
-                    Simple, flexible, and powerful. All it takes are boards,
-                    lists, and cards to get a clear view of who's doing what and
-                    what needs to get done. Learn more in our{' '}
-                    <a>guide for getting started</a>.
-                </p>
+                <div className="intro">
+                    <p className="home-section-title">Krello 101</p>
+                    <h2>A productivity powerhouse</h2>
+                    <p>
+                        Simple, flexible, and powerful. All it takes are boards,
+                        lists, and cards to get a clear view of who's doing what
+                        and what needs to get done. Learn more in our{' '}
+                        <a>guide for getting started</a>.
+                    </p>
+                </div>
 
                 <div className="carousel-and-btns">
                     <div className="carousel-btns">
