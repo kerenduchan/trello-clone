b6726eeda39dfe31c06957a79564968b062c0623
Keren Duchan <keren.duchan@gmail.com>
2024-01-11 17:20:13 +0200

    cover menu sizes: styling

--- src/cmp/task/cover/TaskCoverMenuSize.jsx
+++ src/cmp/task/cover/TaskCoverMenuSize.jsx
@@ -5,6 +5,10 @@ export function TaskCoverMenuSize({ hierarchy, onSizeClick }) {
         return task.cover?.bgColor?.color
     }
 
+    function getTextColor() {
+        return task.cover?.bgColor?.textColor
+    }
+
     return (
         <div className="task-cover-menu-size">
             <ul>
@@ -15,24 +19,50 @@ export function TaskCoverMenuSize({ hierarchy, onSizeClick }) {
                         style={{ backgroundColor: getColor() }}
                     />
                     <div className="contents">
-                        <div className="mock-row" />
-                        <div className="mock-row mock-row-2" />
+                        <div
+                            className={`mock-row ${
+                                getColor() ? '' : 'no-cover'
+                            }`}
+                        />
+                        <div
+                            className={`mock-row mock-row-2 ${
+                                getColor() ? '' : 'no-cover'
+                            }`}
+                        />
                         <div className="mock-row-3">
-                            <div className="mock-small" />
-                            <div className="mock-small" />
-                            <div className="mock-circle" />
+                            <div
+                                className={`mock-small ${
+                                    getColor() ? '' : 'no-cover'
+                                }`}
+                            />
+                            <div
+                                className={`mock-small ${
+                                    getColor() ? '' : 'no-cover'
+                                }`}
+                            />
+                            <div
+                                className={`mock-circle ${
+                                    getColor() ? '' : 'no-cover'
+                                }`}
+                            />
                         </div>
                     </div>
                 </li>
 
                 {/* Large cover */}
                 <li
-                    className="size-large"
+                    className="size-large cover"
                     style={{ backgroundColor: getColor() }}
                 >
                     <div className="contents">
-                        <div className="mock-row" />
-                        <div className="mock-row mock-row-2" />
+                        <div
+                            className="mock-row"
+                            style={{ backgroundColor: getTextColor() }}
+                        />
+                        <div
+                            className="mock-row mock-row-2"
+                            style={{ backgroundColor: getTextColor() }}
+                        />
                     </div>
                 </li>
             </ul>
