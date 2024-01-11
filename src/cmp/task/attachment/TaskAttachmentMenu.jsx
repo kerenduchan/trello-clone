3deffdf5c909143a8fd3e3b407c681bfebf0a968
Keren Duchan <keren.duchan@gmail.com>
2024-01-11 12:16:15 +0200

    wip choose file

--- src/cmp/task/attachment/TaskAttachmentMenu.jsx
+++ src/cmp/task/attachment/TaskAttachmentMenu.jsx
@@ -1,6 +1,17 @@
+import { useRef } from 'react'
 import { PopoverMenu } from '../../general/PopoverMenu'
 
 export function TaskAttachmentMenu({ hierarchy, popoverState }) {
+    const inputFileRef = useRef(null)
+
+    function onClick() {
+        inputFileRef.current.click()
+    }
+
+    function onFileSelected(ev) {
+        console.log('on change', ev)
+    }
+
     return (
         <PopoverMenu
             className="task-attachment-menu"
@@ -9,9 +20,13 @@ export function TaskAttachmentMenu({ hierarchy, popoverState }) {
         >
             <h5>Attach a file from your computer</h5>
 
-            <button className="btn-secondary-centered btn-choose-file">
+            <button
+                className="btn-secondary-centered btn-choose-file"
+                onClick={onClick}
+            >
                 Choose a file
             </button>
+            <input type="file" ref={inputFileRef} onChange={onFileSelected} />
         </PopoverMenu>
     )
 }
