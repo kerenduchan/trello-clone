1ee4016e36d95b4bdaa86caeb210dda206f39ea4
Keren Duchan <keren.duchan@gmail.com>
2024-01-12 13:03:37 +0200

    full cover with image in task preview

--- src/cmp/task/TaskPreviewFullCover.jsx
+++ src/cmp/task/TaskPreviewFullCover.jsx
@@ -1,15 +1,28 @@
 export function TaskPreviewFullCover({ hierarchy }) {
     const { task } = hierarchy
-    const { cover } = task
-    return (
-        <div
-            className="task-preview-full-cover"
-            style={{
+
+    function getStyle() {
+        const { cover } = task
+
+        if (cover.bgColor) {
+            return {
                 backgroundColor: cover.bgColor.color,
                 color: cover.textColor,
-            }}
-        >
-            <div className="title">{task.title}</div>
+            }
+        }
+
+        return {
+            backgroundImage: `url("${cover.bgImage.url}")`,
+        }
+    }
+
+    return (
+        <div className="task-preview-full-cover" style={getStyle()}>
+            <div className="title-container">
+                <div className="title" style={{ color: '#ffffff' }}>
+                    {task.title}
+                </div>
+            </div>
         </div>
     )
 }
