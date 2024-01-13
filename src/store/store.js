d55e25869d4ca361a5191c0530783fdd937e245b
Keren Duchan <74910518+kerenduchan@users.noreply.github.com>
2024-01-13 09:39:31 +0200

    use configureStore from reduxjs/toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,11 @@
-import {
-    combineReducers,
-    compose,
-    legacy_createStore as createStore,
-} from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
+
 import { appReducer } from './reducers/app.reducer'
 import { boardReducer } from './reducers/board.reducer'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-const rootReducer = combineReducers({
-    appModule: appReducer,
-    boardModule: boardReducer,
+export const store = configureStore({
+    reducer: {
+        appModule: appReducer,
+        boardModule: boardReducer,
+    },
 })
-
-export const store = createStore(rootReducer, composeEnhancers())
-
-window.gStore = store
