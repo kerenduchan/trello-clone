33933ef3383eef37e04b92cebf5c87d1651c90e0
Keren Duchan <keren.duchan@gmail.com>
2024-01-14 10:13:45 +0200

    refactor: rename variable

--- src/store/actions/task/task.label.actions.js
+++ src/store/actions/task/task.label.actions.js
@@ -9,15 +9,15 @@ async function addTaskLabel(hierarchy, label) {
     const taskLabelIds = [...task.labelIds, label._id]
 
     // keep the same order of labels as in the board labels
-    const orderedTaskLabelIds = board.labels
+    const labelIds = board.labels
         .filter((l) => taskLabelIds.includes(l._id))
         .map((l) => l._id)
 
-    return updateTask(hierarchy, { labelIds: orderedTaskLabelIds })
+    return updateTask(hierarchy, { labelIds })
 }
 
 async function removeTaskLabel(hierarchy, label) {
     const { task } = hierarchy
-    const taskLabelIds = task.labelIds.filter((id) => id !== label._id)
-    return updateTask(hierarchy, { labelIds: taskLabelIds })
+    const labelIds = task.labelIds.filter((id) => id !== label._id)
+    return updateTask(hierarchy, { labelIds })
 }
