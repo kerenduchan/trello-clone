004250386a62ac94db4f676f3a683477006ea17b
Keren Duchan <keren.duchan@gmail.com>
2024-01-16 11:49:39 +0200

    group axios service

--- frontend/src/services/group/group.service.js
+++ frontend/src/services/group/group.service.js
@@ -1,6 +1,10 @@
+import { groupAxiosService } from './group.axios.service'
 import { groupLocalService } from './group.local.service'
 
-const service = groupLocalService
+// change to true in order to work with local storage instead of server
+const USE_LOCAL = false
+
+const service = USE_LOCAL ? groupLocalService : groupAxiosService
 
 export const groupService = {
     ...service,
