834c1bf7aa8b4deefed2ad0a2482727f90a979c0
Keren Duchan <keren.duchan@gmail.com>
2024-01-18 10:45:32 +0200

    fixing render errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/trello-clone',
     plugins: [react()],
     build: {
         outDir: '../backend/public',
