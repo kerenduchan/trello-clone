bff939fce9ad11f1618d451d2fd33af5cd747191
Keren Duchan <keren.duchan@gmail.com>
2024-01-18 10:31:32 +0200

    build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/trello-clone',
-    outDir: '../backend/public',
     plugins: [react()],
+    build: {
+        outDir: '../backend/public',
+        emptyOutDir: true,
+    },
 })
