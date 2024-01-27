291fd3e01c3bb28a7359e15c9179770cd95e6462
Keren Duchan <keren.duchan@gmail.com>
2024-01-27 09:42:29 +0200

    fix small cover in task preview

--- frontend/src/cmp/task/TaskPreviewCover.jsx
+++ frontend/src/cmp/task/TaskPreviewCover.jsx
@@ -1,30 +1,23 @@
 export function TaskPreviewCover({ hierarchy }) {
     const { task } = hierarchy
+    const { cover } = task
 
     if (!hasCover()) return <></>
 
     function hasCover() {
-        const cover = task.cover
         return cover?.bgImage || cover?.bgColor
     }
 
-    function getStyle() {
-        const { bgColor, bgImage } = task.cover
-
-        if (bgColor) {
-            return { backgroundColor: bgColor.color }
-        }
-
-        var img = new Image()
-
-        return {
-            backgroundImage: `url(${bgImage.url})`,
-            backgroundSize: 'cover',
-            backgroundPosition: '50%',
-            height: '250px',
-            backgroundRepeat: 'no-repeat',
-        }
-    }
-
-    return <div className="task-preview-cover" style={getStyle()}></div>
+    return (
+        <div className="task-preview-cover">
+            {cover.bgColor ? (
+                <div
+                    className="bg-color"
+                    style={{ backgroundColor: cover.bgColor.color }}
+                />
+            ) : (
+                <img className="bg-image" src={cover.bgImage.url} />
+            )}
+        </div>
+    )
 }
